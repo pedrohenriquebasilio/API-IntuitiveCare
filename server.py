615928f873092226540ea59eca10065e6a417691
Pedro <phbasilio6903@gmail.com>
2025-04-01 14:36:39 -0300

    Feature: funcao para fazer metrica de relevancia

--- server.py
+++ server.py
@@ -14,3 +14,14 @@ async def load_data():
     global operadoras_df
     operadoras_df = pd.read_csv("Relatorio_cadop.csv", sep=";", encoding="utf-8")
     operadoras_df = operadoras_df.fillna("")
+    def calculate_relevance(row, query):
+    score = 0
+    query_lower = query.lower()
+    
+    for field in ["Razao_Social", "Nome_Fantasia", "Cidade", "UF"]:
+        if query_lower in str(row[field]).lower():
+            score += 1
+            
+            if str(row[field]).lower().startswith(query_lower):
+                score += 2
+    return score
